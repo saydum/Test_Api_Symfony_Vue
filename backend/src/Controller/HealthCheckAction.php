b685a399aa47921c65c1e702fe27fc1b0ee19cb8
Saydum Halibekov <saydum.it5@gmail.com>
2022-06-18 15:43:57 +0300

    Create test class and UnitTest

--- backend/src/Controller/HealthCheckAction.php
+++ backend/src/Controller/HealthCheckAction.php
@@ -2,7 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/health-check', name: 'health_check', methods: ['GET'])]
 class HealthCheckAction
 {
-
+    public function __invoke(): Response
+    {
+        return new JsonResponse(['status' => 'ok']);
+    }
 }
